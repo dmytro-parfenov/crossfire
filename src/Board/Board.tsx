33756f32ebbd8ce15f76095bf16e6271c24f95d4
dmitryparfenov937 <dmitryparfenov937@gmail.com>
2025-11-24 20:21:23 +0000

    add board click handler

--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -1,5 +1,9 @@
-import type { FC, PropsWithChildren } from "react";
+import { useCallback, useEffect, useRef, type FC, type PointerEventHandler, type PropsWithChildren } from "react";
 import './Board.css';
+
+const resolveSection = (sections: number, size: number, pointer: number) => {
+    return Math.floor(pointer / (size / sections)) + 1;
+}
 
 type Props = {
     rows: number;
@@ -7,6 +11,27 @@ type Props = {
     onClick?: (row: number, col: number) => void;
 }
 
-export const Board: FC<PropsWithChildren<Props>> = ({ rows, cols, children }) => {
-    return <div className="board" style={{ '--board-rows': rows, '--board-cols': cols }}>{children}</div>
+export const Board: FC<PropsWithChildren<Props>> = ({ rows, cols, onClick, children }) => {
+    const boardElementRef = useRef<HTMLDivElement>(null);
+
+    /**
+     * TODO: move to the hook
+     */
+    const handleClick = useCallback<PointerEventHandler<HTMLDivElement>>((event) => {
+        if (!boardElementRef.current) {
+            return;
+        }
+
+        const { width, height, top, left } = boardElementRef.current.getBoundingClientRect();
+
+        const x = event.nativeEvent.clientX - left;
+        const y = event.nativeEvent.clientY - top;
+
+        const row = resolveSection(rows, height, y);
+        const col = resolveSection(cols, width, x);
+
+        onClick?.(row, col);
+    }, [rows, cols, onClick]);
+
+    return <div className="board" ref={boardElementRef} style={{ '--board-rows': rows, '--board-cols': cols }} onClick={handleClick}>{children}</div>
 }
